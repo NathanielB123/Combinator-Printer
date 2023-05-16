ed5cc4bd758bd5499d131670d3aa44d2baf454e7
Nathaniel Burke <nrb21@ic.ac.uk>
2023-05-16 17:07:22 +0100

    Improve printing
    We now convert to an intermediary lambda calaculus AST `L`
    
    This allows us to easily print with correct parenthesis and perform
    eta-reduction

--- src/CombinatorPrinter.hs
+++ src/CombinatorPrinter.hs
@@ -8,13 +8,19 @@ module CombinatorPrinter
   , Tree(..)
   , T2T
   , STree
-  , showS
   , CreateSTree(..)
   , FillFun(..)
+  , toL
+  , Pretty(..)
+  , L
+  , breduce
+  , ereduce
+  , showS
   ) where
 
 import Control.Monad.State (MonadState(..), State, evalState)
 import Data.Char (chr, ord)
+import Data.Foldable (Foldable(..))
 import Data.List (intercalate)
 
 -- | Print a combinator
@@ -31,8 +37,15 @@ printCombinator = putStrLn . showCombinator
 --
 -- Arbitrary combinators can be transformed into this form be simply 
 -- instantiating every type variable with `Tree`
+--
+-- Possible future improvements for neater printing:
+--
+-- * Replace unused variables with "@_@" instead of giving them explicit names
+--
+-- * Introduce some mechanism to keep creating unique valid variable names past 
+-- @26/"z"@. For example, appending numbers
 showCombinator :: CreateSTree f => f -> String
-showCombinator x = evalState (showS $ stree x) 0
+showCombinator x = pretty . ereduce $ evalState (toL $ stree x) 0
 
 -- | Example combinator
 foo :: (p1 -> p1) -> (p1 -> (p1 -> p1) -> p1) -> p2 -> p1 -> p1
@@ -41,7 +54,7 @@ foo = \x y _ z -> y z (\w -> (x (x (y w (\_ -> x w)))))
 -- | Like `foo` but with all type variables instantiated to `Tree`
 --
 -- >>> showCombinator foo'
--- "(\\a b c d -> b (d) (\\e -> a (a (b (e) (\\f -> a (e))))))"
+-- "\\a b c d -> b d (\\e -> a (a (b e (\\f -> a e))))"
 foo' ::
      (Tree -> Tree) -> (Tree -> (Tree -> Tree) -> Tree) -> Tree -> Tree -> Tree
 foo' = foo
@@ -52,8 +65,8 @@ type T2T = Tree -> Tree
 -- | We can instantiate the polymorphic type variables with alternative types
 -- containing just `(->)`s and `Tree`s. For example, "@Tree -> Tree@".
 --
--- This will print a different (more complicated) combinator but still works
--- (and if we performed beta-reduction, I think it would print the same thing)
+-- After performing `ereduce`-ing, we will obtain the same `L` and so will
+-- print to the same combinator
 foo'' :: (T2T -> T2T) -> (T2T -> (T2T -> T2T) -> T2T) -> T2T -> T2T -> T2T
 foo'' = foo
 
@@ -65,34 +78,98 @@ type STree = State Int Tree
 data Tree =
   Node Int [STree]
 
-munless :: Monoid c => Bool -> c -> c
-munless True = const mempty
-munless False = id
+-- | Simple lambda calculus AST
+data L
+  = Lam Int L
+  | App L L
+  | Term Int
+  deriving (Show)
 
--- | Traverse the built-up tree and print the lambdas and applications
---
--- Some improvements could be made here for neater printing:
---
--- * Remove unncessary parens
---
--- * Replace unused variables with "@_@" instead of giving them explicit names
---
--- * Perform beta-reduction. i.e: "@\a -> f (a)@" should really just be printed
--- as "@f@"
-showS :: STree -> State Int String
-showS t = do
+-- | Pretty-printing
+class Pretty a where
+  pretty :: a -> String
+
+enclose :: String -> String -> String -> String
+enclose l r s = l <> s <> r
+
+parensEnclose :: String -> String
+parensEnclose = enclose "(" ")"
+
+data PrettyState
+  = WasLam
+  | NoParens
+  | Parens
+  deriving (Eq)
+
+instance Pretty L where
+  pretty :: L -> String
+  pretty = go NoParens
+    where
+      addParens Parens = parensEnclose
+      addParens _ = id
+      wasLam WasLam = True
+      wasLam _ = False
+      go :: PrettyState -> L -> String
+      go ps (Lam x l) =
+        addParens ps $ munless (wasLam ps) "\\" <> toChr x <> " " <> go WasLam l
+      go ps (App l1 l2) =
+        addParens ps $
+        mwhen (wasLam ps) "-> " <> go NoParens l1 <> " " <> go Parens l2
+      go _ (Term x) = toChr x
+
+mwhen :: Monoid a => Bool -> a -> a
+mwhen True = id
+mwhen False = const mempty
+
+munless :: Monoid c => Bool -> c -> c
+munless = mwhen . not
+
+toChr :: Int -> [Char]
+toChr i = [chr $ (i + ord 'a')]
+
+-- | Traverse the built-up tree and convert into the lambda calculus ADT `L`
+toL :: STree -> State Int L
+toL t = do
   cur <- get
   (Node i xs) <- t
   new <- get
-  xs' <- traverse showS xs
-  let r = [cur .. new - 1]
-  pure $
-    "(" <>
-    munless (null r) ("\\" <> intercalate " " (fmap toChr r) <> " -> ") <>
-    toChr i <> munless (null xs') (" " <> intercalate " " xs') <> ")"
-  where
-    toChr i = [chr $ (i + ord 'a')]
+  xs' <- traverse toL xs
+  pure $ foldr Lam (foldl' App (Term i) xs') [cur .. new -1]
 
+replaceIn :: Int -> L -> L -> L
+replaceIn x y t@(Term x')
+  | x == x' = y
+  | otherwise = t
+replaceIn x y (Lam i l) = Lam i $ replaceIn x y l
+replaceIn x y (App l1 l2) = App (replaceIn x y l1) (replaceIn x y l2)
+
+occurIn :: Int -> L -> Bool
+occurIn i (Term i') = i == i'
+occurIn i (App l1 l2) = occurIn i l1 || occurIn i l2
+-- Technically, we don't need to check the name of the bound variable here
+-- for `L`s we create as we always guarantee variables will not share names,
+-- but the additional code is very small
+occurIn i (Lam i' l) = i /= i' && occurIn i l
+
+-- | Reduce via eta reduction
+ereduce :: L -> L
+ereduce t@(Lam i (App l (Term i')))
+  | i == i' && not (occurIn i l) = ereduce l
+  | otherwise = t
+ereduce (Lam i l) = Lam i $ ereduce l
+ereduce (App l1 l2) = App (ereduce l1) (ereduce l2)
+ereduce t@Term {} = t
+
+-- | Reduce via beta reduction
+--
+-- Not actually used
+breduce :: L -> L
+breduce t@Term {} = t
+breduce (Lam x l) = Lam x (breduce l)
+breduce (App (Lam x l) y) = breduce $ replaceIn x y l
+breduce (App l1 l2) = App (breduce l1) (breduce l2)
+
+-- We need to fold over r, turning the (:)s into Lams
 fresh :: State Int Int
 fresh = do
   x <- get
@@ -136,3 +213,22 @@ instance FillFun Tree where
 instance (CreateSTree f, FillFun g) => FillFun (f -> g) where
   fill :: ([STree] -> Tree) -> f -> g
   fill n f = fill @g $ \xs -> n (stree f : xs)
+
+--------------------------------------------------------------------------------
+-- Old:
+--------------------------------------------------------------------------------
+-- | Traverse the built-up tree and print the lambdas and applications directly
+--
+-- Lower quality output compare to using `toL` and `pretty` (more parens than
+-- necessary)
+showS :: STree -> State Int String
+showS t = do
+  cur <- get
+  (Node i xs) <- t
+  new <- get
+  xs' <- traverse showS xs
+  let r = [cur .. new - 1]
+  pure $
+    "(" <>
+    munless (null r) ("\\" <> intercalate " " (fmap toChr r) <> " -> ") <>
+    toChr i <> munless (null xs') (" " <> intercalate " " xs') <> ")"
